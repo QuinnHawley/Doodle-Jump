ded2f08383cd5d618317c8b9ba5e7c95e74b7b62
rprandeski123 <staticchucc@outlook.com>
2019-05-24 13:26:12 -0400

    h

--- Doodle Jump/GameScene.swift
+++ Doodle Jump/GameScene.swift
@@ -19,18 +19,7 @@ class GameScene: SKScene
         borderBody.restitution = 1
         physicsBody = borderBody
         physicsWorld.gravity = CGVector(dx: 0, dy: -9.8)
-        
-        
-        player = SKSpriteNode(color: .red, size: CGSize(width: 100, height: 100))
-        player.position = CGPoint(x: frame.width * 0.5, y: frame.height * 0.8)
-        player.physicsBody?.mass = 0.5
-        player.physicsBody?.velocity = CGVector(dx: 500, dy: 500)
-        physicsBody?.affectedByGravity = true
-        physicsBody?.allowsRotation = true
-        physicsBody?.isDynamic = true
-        physicsBody?.restitution = 0.0
-        physicsBody?.friction = 0.0
-        addChild(player)
+    
         
         
         
